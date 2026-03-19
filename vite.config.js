5f490ddddd55b1762d4521c737475e43abda14b2
roberto <rob.davy178@gmail.com>
2026-03-19 23:12:17 +0800

    asd

--- vite.config.js
+++ vite.config.js
@@ -2,6 +2,7 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 
 export default defineConfig({
+  base: '/Happy-Birthday/',
   plugins: [react()],
   assetsInclude: ['**/*.MOV', '**/*.MP4'],
 })
